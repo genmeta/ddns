1dd4ed51482329dc2e5cbc8b96363bd4b981b88f
eareimu <reimu@genmeta.net>
2026-06-16 15:59:21 +0800

    chore: remove legacy ddns files

--- src/bin/ddns-server/lookup/mod.rs
+++
@@ -1,8 +0,0 @@
-mod http;
-pub(crate) mod query;
-mod ranking;
-
-pub use http::{LookupSvc, Request, Response, body_response, write_error};
-
-#[cfg(test)]
-mod tests;
--- src/bin/ddns-server/publish/mod.rs
+++
@@ -1,7 +0,0 @@
-mod http;
-mod store;
-
-pub use http::PublishSvc;
-
-#[cfg(test)]
-mod tests;
